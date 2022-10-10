70576d92d661680f6fe1573fcb5969686c548bd3
tejasvijb <jbtejasvi@gmail.com>
2022-10-11 01:11:16 +0530

    getGenres

--- files/public/script.js
+++ files/public/script.js
@@ -2,8 +2,23 @@ const tmdbKey = '908b84de69d3d9b47d16b0f4d4424744';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list'
+  const requestParams = `?api_key=${tmdbKey}`
 
+  const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams
+
+  try{
+    const response = await fetch(urlToFetch)
+    if(response.ok){
+      const jsonResponse = await response.json()
+      const genres = jsonResponse.genres
+      return genres
+    }
+  }
+  catch(error){
+    console.log(error)
+  }
 };
 
 const getMovies = () => {
