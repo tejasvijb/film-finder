51290f4a528aa1093e586340150dc98ba4896556
tejasvijb <jbtejasvi@gmail.com>
2022-10-11 01:43:34 +0530

    showRandomMovie

--- files/public/script.js
+++ files/public/script.js
@@ -63,11 +63,16 @@ const getMovieInfo = async (movie) => {
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = () => {
+const showRandomMovie = async () => {
   const movieInfo = document.getElementById('movieInfo');
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
   };
+
+  const movies = await getMovies()
+  const randomMovie = getRandomMovie(movies)
+  const info = await getMovieInfo(randomMovie)
+  displayMovie(info)
 
 };
 
