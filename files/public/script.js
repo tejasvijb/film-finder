972bf95e057a8f9b81553811db8d641d64d1c262
tejasvijb <jbtejasvi@gmail.com>
2022-10-11 01:12:34 +0530

    getMovies

--- files/public/script.js
+++ files/public/script.js
@@ -21,8 +21,25 @@ const getGenres = async () => {
   }
 };
 
-const getMovies = () => {
+const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie'
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`
+  
+  const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams
+  
+  try{
+    const response = await fetch(urlToFetch)
+    if(response.ok){
+      const jsonResponse = await response.json()
+      const movies = jsonResponse.results
+      return movies
+    }
+  }
+  catch(error){
+    console.log(error)
+  }
+
 
 };
 
